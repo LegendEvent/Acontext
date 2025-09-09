f02e22efe8e82e38e97a73935019a7aa08b6989f
Gus <jianbaiye@outlook.com>
2025-09-09 14:53:05 +0800

    chore: update minio default envs

--- src/server/scripts/local_dev_deps.sh
+++ src/server/scripts/local_dev_deps.sh
@@ -2,7 +2,13 @@ set -e
 rm -rf ./db/dev_data
 rm -rf ./db/dev_redis
 rm -rf ./db/dev_rabbitmq
-DATABASE_LOCATION="./db/dev_data" REDIS_LOCATION="./db/dev_redis" RABBITMQ_LOCATION="./db/dev_rabbitmq" docker compose up acontext-server-pg acontext-server-redis acontext-server-rabbitmq
+rm -rf ./db/dev_s3
+DATABASE_LOCATION="./db/dev_data" \
+REDIS_LOCATION="./db/dev_redis" \
+RABBITMQ_LOCATION="./db/dev_rabbitmq" \
+S3_LOCATION="./db/dev_s3" \
+docker compose up acontext-server-pg acontext-server-redis acontext-server-rabbitmq acontext-server-minio-setup acontext-server-minio
 rm -rf ./db/dev_data
 rm -rf ./db/dev_redis
 rm -rf ./db/dev_rabbitmq
+rm -rf ./db/dev_s3
